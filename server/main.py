04bde336cb347eafe6415c5d0a1ed63b4468d89a
Vlados <89387622+nebel310@users.noreply.github.com>
2025-03-24 00:02:44 +0300

    клоп

--- server/main.py
+++ server/main.py
@@ -65,7 +65,7 @@ app.include_router(courses_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],  # Тут адрес фронтенда
+    allow_origins=["http://127.0.0.1:3000"],  # Тут адрес фронтенда
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
